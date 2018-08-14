b88c5c19292d7fbaafe92debd383fd2cc12f5fe5
Nael Mansi <naelmansi@gmail.com>
2018-08-14 15:38:39 +0000

    after doing the hello world

--- django_fs_env/urls.py
+++ django_fs_env/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from first_app import views
+from django.conf.urls import url
 
 urlpatterns = [
+    url(r'^$',views.index, name='index'),
     path('admin/', admin.site.urls),
 ]
